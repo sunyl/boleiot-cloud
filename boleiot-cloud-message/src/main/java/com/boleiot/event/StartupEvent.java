7e72995a1a79ed14e71d58f9ee87eaabcc98943d
sunyl <335950550@qq.com>
2018-01-04 14:45:20 +0800

    修改udp通讯

--- boleiot-cloud-message/src/main/java/com/boleiot/event/StartupEvent.java
+++ boleiot-cloud-message/src/main/java/com/boleiot/event/StartupEvent.java
@@ -2,28 +2,23 @@ package com.boleiot.event;
 
 import com.boleiot.config.SysConfig;
 import com.boleiot.udp.UdpServer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 
 public class StartupEvent implements ApplicationListener<ContextRefreshedEvent> {
-    private static final Logger log = LoggerFactory.getLogger(StartupEvent.class);
 
     private static ApplicationContext context;
 
     @Override
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
-
         try {
             context = contextRefreshedEvent.getApplicationContext();
             SysConfig sysConfig = context.getBean(SysConfig.class);
             UdpServer udpServer = (UdpServer) StartupEvent.getBean(UdpServer.class);
             udpServer.run(sysConfig.getUdpReceivePort());
-
         } catch (Exception e) {
-            log.error("Exception", e);
+            e.printStackTrace();
         }
     }
 
